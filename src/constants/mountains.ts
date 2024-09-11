4fdd2b546a660ea985ae9de1f35b13ebc31ae055
k-daiguji <hikatteru20@gmail.com>
2024-09-11 22:16:22 +0900

    Add k2

--- src/constants/mountains.ts
+++ src/constants/mountains.ts
@@ -1,11 +1,12 @@
 import { Everest } from "../models/mountains/everest";
 import { Fuji } from "../models/mountains/fuji";
+import { K2 } from "../models/mountains/k2";
 
 export const MOUNTAINS = [
   new Everest(),
+  new K2(),
   new Fuji(),
   // TODO: Change unique characters
-  new Fuji(),
   new Fuji(),
   new Fuji(),
   new Fuji(),
---
+++ src/models/mountains/k2.ts
@@ -0,0 +1,13 @@
+import { BaseMountain } from "./mountain";
+
+export class K2 extends BaseMountain {
+  constructor() {
+    super(8611, 180);
+  }
+
+  public getName = () => "K2";
+
+  public getPath = () => "./dist/datasets/K2.png";
+
+  public getHeight = () => "86%";
+}
