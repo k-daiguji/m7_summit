922cb255fcafc6eb92d379bc0ffb4979628c348b
k-daiguji <hikatteru20@gmail.com>
2024-09-11 23:22:44 +0900

    Add kilimanjaro

--- src/constants/mountains.ts
+++ src/constants/mountains.ts
@@ -1,15 +1,16 @@
 import { Everest } from "../models/mountains/everest";
 import { Fuji } from "../models/mountains/fuji";
 import { K2 } from "../models/mountains/k2";
+import { Kilimanjaro } from "../models/mountains/kilimanjaro";
 import { Matterhorn } from "../models/mountains/matterhorn";
 
 export const MOUNTAINS = [
   new Everest(),
   new K2(),
+  new Kilimanjaro(),
   new Matterhorn(),
   new Fuji(),
   // TODO: Change unique characters
-  new Fuji(),
   new Fuji(),
   new Fuji(),
 ] as const;
